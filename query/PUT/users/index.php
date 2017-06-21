86b8747b2f44f4021519b7a94e85b6a678546f32
Elfennacht <christian.boisson@epitech.eu>
2017-06-21 12:39:45 +0200

    PUT /users/ - cleaning, language, authentication
    Fix:
    - Now read the query body as JSON and not URL-encoded
    Delete:
    - Removing unnecessary code
    Add:
    - Adding laguage support
    - Adding Authentication

--- query/PUT/users/index.php
+++ query/PUT/users/index.php
@@ -1,50 +1,39 @@
 <?php
+	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Authentication.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Response.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Exception.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/class/User.php');
 
-	function getRealPOST()
-	{
-		$pairs = explode("&", file_get_contents("php://input"));
-		$vars = array();
-		foreach ($pairs as $pair)
-		{
-			$nv = explode("=", $pair);
-			$name = urldecode($nv[0]);
-			$value = urldecode($nv[1]);
-			$vars[$name] = $value;
-		}
-		return $vars;
-	}
-
 	$response = new Response(Response::OK);
 	try {
-		$post = getRealPOST();
+		$post = json_decode(file_get_contents('php://input'));
+		
 		$user = new User();
 
 		if (isset($_GET['accept']))
 			$response->setContentType($_GET['accept']);
-		if (($id = (isset($_GET['id']) ? 'id' : (isset($_GET['nickname']) ? 'nickname' : false))) === false)
+		$authentication = new Authentication();
+		$authentication->verify();
+
+		if (!isset($_GET['id']))
 			throw new ParametersException("Missing parameters to proceed", Response::MISSPARAM);
-		if (!($id == "id" ? $user->getFromID($_GET[$id]) : $user->getFromNickname($_GET[$id]));
-			throw new ParametersException("This user does not exist", Response::DOESNOTEXIST);
-		if (!isset($post['nickname']) or !isset($post['password']) or !isset($post['email']))
+		$user = $authentication->getUserFromToken();
+		
+		if (!$user)
+			throw new UnknownException("Something wrong happened");
+		
+		if ($_GET['id'] != $user->ID) //Add moderator/admin detection here
+			throw new ParametersException("You cannot modify someone else's account", Response::NORIGHT);
+
+		if (!isset($post->name) or !isset($post->password)
+			or !isset($post->email) or !isset($post->language))
 			throw new ParametersException("Missing parameters to proceed", Response::MISSPARAM);
-		$user->setName($post['nickname']);
-		$user->setEmail($post['email']);
-		$user->setPassword($post['password']);
-		// if (isset($post['country']))
-			// $user->setCountry((int)$post['country']);
-		// else
-			// $user->setCountry(null);
-		// if (isset($post['birthdate']))
-			// $user->setBirthdate($post['birthdate']);
-		// else
-			// $user->setBirthdate(null);
-		// if (isset($post['rank']))
-			// $user->setRank((int)$post['rank']);
-		// else
-			// $user->setRank(null);
+
+		$user->setName($post->name);
+		$user->setEmail($post->email);
+		$user->setPassword($post->password);
+		$user->setLanguage($post->language->code);
+
 		if (!$user->update())
 			throw new UnknownException("Something wrong happened", Response::UNKNOWN);
 		$response->setMessage(["message" => "User overwrited successfully"], Response::SUCCESS);
