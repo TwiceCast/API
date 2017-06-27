4691c0ce63610a2b69e1e7e9b7fce65c66f79e92
Elfennacht <christian.boisson@epitech.eu>
2017-06-27 18:10:02 +0200

    PATCH /users/ - Fully recast

--- query/PATCH/users/users.php
+++ query/PATCH/users/users.php
@@ -1,72 +1,38 @@
 <?php
+	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Authentication.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Response.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Exception.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/class/User.php');
 
-	function getRealPOST()
-	{
-		$pairs = explode("&", file_get_contents("php://input"));
-		$vars = array();
-		foreach ($pairs as $pair)
-		{
-			$nv = explode("=", $pair);
-			$name = urldecode($nv[0]);
-			$value = urldecode($nv[1]);
-			$vars[$name] = $value;
-		}
-		return $vars;
-	}
-
 	$response = new Response(Response::OK);
 	try {
-		$post = getRealPOST();
-		$user = new User();
+		$post = getPostData();
 
 		if (isset($_GET['accept']))
 			$response->setContentType($_GET['accept']);
-		if (($id = (isset($_GET['id']) ? 'id' : (isset($_GET['nickname']) ? 'nickname' : false))) === false)
-			throw new ParametersException("Missing parameters to proceed", Response::MISSPARAM);
-		$user = ($id == "id" ? $user->getFromID($_GET[$id]) : $user->getFromNickname($_GET[$id]));
-		if (!$user)
-			throw new ParametersException("This user does not exist", Response::DOESNOTEXIST);
-
-		// For this part, we need to check all paramters like:
-		//   - email: Must be valid and unique
-		//   - password: Must pass minimal requierement
-		//   - nickname: Must be valid and unique
-		//   - country: Must exist
-		//   - rank: Must exist
+		$authentication = new Authentication();
+		$authentication->verify();
 		
-		// If there is too much SQL Query here we can stop using cangeXXX() function and use setXXX() then update()
-		$out = array();
+		if (!isset($_GET['id']))
+			throw new ParametersException("Missing parameters to proceed", Response::MISSPARAM);
+		$user = $authentication->getUserFromToken();
+		
+		if (!$user)
+			throw new UnknownException("Something wrong happened");
+		
+		if ($_GET['id'] != $user->ID) //add moderator/admin detection here
+			throw new ParametersException("You cannot modify someone else's account", Response::NORIGHT);
+		
 		if (isset($post['email']))
-			$out['email'] = $user->changeEmail($post['email']);
+			$user->changeEmail($post['email']);
 		if (isset($post['password']))
-			$out['password'] = $user->changePassword($post['password']);
+			$user->changePassword($post['password']);
 		if (isset($post['nickname']))
-			$out['nickname'] = $user->changeName($post['nickname']);
-		// if (isset($post['country']))
-		// {
-			// if ($user->changeCountry($post['country']))
-				// $out[] = array('country', Err::SUCCESS);
-			// else
-				// $out[] = array('country', Err::UNKNOW);
-		// }
-		// if (isset($post['birthdate']))
-		// {
-			// if ($user->changeBirthdate($post['birhdate']))
-				// $out[] = array('birthdate', Err::SUCCESS);
-			// else
-				// $out[] = array('birthdate', Err::UNKNOW);
-		// }
-		// if (isset($post['rank']))
-		// {
-			// if ($user->changeRank($post['rank']))
-				// $out[] = array('rank', Err::SUCCESS);
-			// else
-				// $out[] = array('rank', Err::UNKNOW);
-		// }
-		$response->setMessage($out);
+			$user->changeName($post['nickname']);
+		if (isset($post['language']))
+			$user->changeLanguage($post['language']['code']);
+		
+		$response->setMessage($user);
 	} catch (CustomException $e) {
 		$response->setError($e);
 	} finally {
