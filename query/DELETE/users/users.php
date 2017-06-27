24aa0fcbebfa23710605d17c77738a90277e1ba5
Elfennacht <christian.boisson@epitech.eu>
2017-06-27 22:37:44 +0200

    DELETE /users/ - Correction
    Add:
    - missing negation in front of delete condition

--- query/DELETE/users/users.php
+++ query/DELETE/users/users.php
@@ -1,7 +1,7 @@
 <?php
 	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Authentication.php');
-	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Exception.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Response.php');
+	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Exception.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/class/User.php');
 
 	$response = new Response(Response::OK);
@@ -21,7 +21,7 @@
 		if ($_GET['id'] != $user->ID) //Add moderator/admin detection here
 			throw new ParametersException("You cannot delete someone else's account", Response::NORIGHT);
 		
-		if ($user->delete())
+		if (!$user->delete())
 			throw new UnknownException("Something wrong happened", Response::UNKNOWN);
 		$response->setMessage("The user has been removed successfully.", Response::SUCCESS);
 	} catch (CustomException $e) {
