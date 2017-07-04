88d0e24d9aa062d89823c219bf2d787f43dc193e
Elfennacht <christian.boisson@epitech.eu>
2017-07-04 22:00:34 +0200

    Class Organisation - Error, nomenclature, language, private, rights
    Delete:
    - Code for advanced permission system put under comment
    Add:
    - Adding Exceptions to handle errors
    - Adding Language support
    - Adding Private support
    Fix:
    - Changing "id" nomenclature to fit documentation

--- class/Organization.php
+++ class/Organization.php
@@ -1,18 +1,22 @@
 <?php
-	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Error.php');
+	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Response.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/class/DB.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/class/User.php');
 
 	class Organization
 	{
-		var $ID;
+		var $id;
 		var $name;
+		var $language;
+		var $private;
 		private $db;
 
-		function __construct($db = true, $ID = null, $name = null)
+		function __construct($db = true, $id = null, $name = null, $language = null, $private = null)
 		{
-			$this->setID($ID);
+			$this->setId($id);
 			$this->setName($name);
+			$this->setLanguage($language);
+			$this->setPrivate($private);
 			if ($db)
 				$this->db = new DB();
 			else
@@ -24,15 +28,32 @@
 			return "organization";
 		}
 
-		function setID($ID)
+		function setId($id)
 		{
-			$this->ID = $ID;
+			$this->id = $id;
 			return $this;
 		}
 
 		function setName($name)
 		{
 			$this->name = $name;
+			return $this;
+		}
+
+		function setLanguage($language)
+		{
+			if (is_object($language))
+				$code = $language->code;
+			else
+				$code = $language;
+			$this->language = new stdClass();
+			$this->language->code = $code;
+			return $this;
+		}
+		
+		function setPrivate($private)
+		{
+			$this->private = (bool) $private;
 			return $this;
 		}
 
@@ -46,22 +67,26 @@
 				return false;
 		}
 
-		function getFromID($ID, $db = null)
+		function getFromId($id, $db = null)
 		{
 			$link = $this->getLink($db);
 			if ($link)
 			{
 				$link->prepare('
-					SELECT organization.id AS organizationID,
-					organization.name AS organizationName
+					SELECT organization.id AS organizationId,
+					organization.name AS organizationName,
+					organization.language, AS organizationLanguage,
+					organization.private AS organizationPrivate
 					FROM organization
-					WHERE organization.id = :ID');
-				$link->bindParam(':ID', $ID, PDO::PARAM_INT);
+					WHERE organization.id = :id');
+				$link->bindParam(':id', $id, PDO::PARAM_INT);
 				$data = $link->fetch(true);
 				if ($data)
 				{
-					$this->setID($data['organizationID']);
-					$this->setname($data['organizationName']);
+					$this->setId($data['organizationId']);
+					$this->setName(DB::fromDB($data['organizationName']));
+					$this->setLanguage(DB::fromDB($data['organizationLanguage']));
+					$this->setPrivate($data['organizationPrivate']);
 					return true;
 				}
 				else
@@ -77,16 +102,20 @@
 			if ($link)
 			{
 				$link->prepare('
-					SELECT organization.id AS organizationID,
-					organization.name AS organizationName
+					SELECT organization.id AS organizationId,
+					organization.name AS organizationName,
+					organization.language AS organizationLanguage,
+					organization.private AS organizationPrivate
 					FROM organization
 					WHERE organization.name = :name');
 				$link->bindParam(':name', $name, PDO::PARAM_STR);
 				$data = $link->fetch(true);
 				if ($data)
 				{
-					$this->setID($data['organizationID']);
-					$this->setname($data['organizationName']);
+					$this->setId($data['organizationId']);
+					$this->setName(DB::fromDB($data['organizationName']));
+					$this->setLanguage(DB::fromDB($data['organizationLanguage']));
+					$this->setPrivate($data['organizationPrivate']);
 					return true;
 				}
 				else
@@ -96,47 +125,63 @@
 				return false;
 		}
 
-		function getAllOrganizations($db = null)
+		function getAllOrganizations($limit = null, $offset = null, $db = null)
 		{
 			$link = $this->getLink($db);
 			if ($link)
 			{
-				$link->prepare('
-					SELECT organization.id AS organizationID,
-					organization.name AS organizationName
-					FROM organization');
-				$data = $link->fetchAll(true);
-				if ($data)
+				$query = '
+					SELECT organization.id AS organizationId,
+					organization.name AS organizationName,
+					organization.language AS organizationLanguage,
+					organization.private AS organizationPrivate
+					FROM organization
+					ORDER BY organization.id';
+				if ($limit)
 				{
-					$organizations = array();
-					foreach ($data as &$entry)
+					$limit = (int) $limit;
+					if ($offset)
 					{
-						$organization = new Organization(false);
-						$organization->setID($entry['organizationID']);
-						$organization->setname($entry['organizationName']);
-						$organizations[] = $organization;
+						$offset = (int) $offset;
+						$query .= " LIMIT ".$limit." OFFSET ".$offset;
 					}
-					return $organizations;
+					else
+						$query .= " LIMIT ".$limit;
 				}
-				else
+				$link->prepare($query);
+				$data = $link->fetchAll(true);
+				if ($data === false)
 					return false;
+				$organizations = array();
+				foreach ($data as &$entry)
+				{
+					$organization = new Organization(false);
+					$organization->setId($entry['organizationId']);
+					$organization->setName(DB::fromDB($entry['organizationName']));
+					$organization->setLanguage(DB::fromDB($entry['organizationLanguage']));
+					$organization->setPrivate($entry['organizationPrivate']);
+					$organizations[] = $organization;
+				}
+				return $organizations;
 			}
 			else
 				return false;
 		}
 
-		function getFromUserID($ID, $db = null)
+		function getFromUserId($id, $db = null)
 		{
 			$link = $this->getLink($db);
 			if ($link)
 			{
 				$link->prepare('
-					SELECT organization.id AS organizationID,
-					organization.name AS organizationName
-					FROM or_user_role
-					LEFT JOIN organization ON or_user_role.id_organization = organization.id
-					WHERE or_user_role.id_user = :ID');
-				$link->bindParam(':ID', $ID, PDO::PARAM_INT);
+					SELECT organization.id AS organizationId,
+					organization.name AS organizationName,
+					organization.language AS organizationLanguage,
+					organization.private AS organizationPrivate
+					FROM client_role
+					LEFT JOIN organization ON client_role.id_target = organization.id
+					WHERE client_role.id_user = :id AND categorie_target = "Organisation"');
+				$link->bindParam(':id', $id, PDO::PARAM_INT);
 				$data = $link->fetchAll(true);
 				if ($data)
 				{
@@ -144,8 +189,10 @@
 					foreach ($data as &$entry)
 					{
 						$organization = new Organization(false);
-						$organization->setID($entry['organizationID']);
+						$organization->setId($entry['organizationId']);
 						$organization->setName(DB::fromDB($entry['organizationName']));
+						$organization->setLanguage(DB::fromDB($entry['organizationLanguage']));
+						$organization->setPrivate($entry['organizationPrivate']);
 						$organizations[] = $organization;
 					}
 					return $organizations;
@@ -157,27 +204,29 @@
 				return false;
 		}
 
-		function getOrganizationMembers($db = null)
+		function getMembers($db = null)
 		{
 			$link = $this->getLink($db);
 			if ($link)
 			{
 				$link->prepare('
-					SELECT client.id AS clientID,
+					SELECT client.id AS clientId,
 					client.email AS clientEmail,
 					client.password AS clientPassword,
 					client.name AS clientName,
-					client.register_data AS clientRegisterDate
+					client.register_data AS clientRegisterDate,
+					client.language AS clientLanguage,
+					client.private AS clientPrivate
 					FROM or_user_role
-					LEFT JOIN client ON or_user_role.id_user = client.id
-					WHERE or_user_role.id_organization = :ID');
+					LEFT JOIN client ON client_role.id_client = client.id
+					WHERE client_role.id_organization = :id AND categorie_target = "Organisation"');
 					// Possibilité d'ajouter
 					// LEFT JOIN or_role ON or_user_role.id_role = or_role.id
 					// pour avoir le rôle de chaque user en même temps.
-					// Possibilité d'ajouter un "UNIQUE" sur le client.ID
+					// Possibilité d'ajouter un "UNIQUE" sur le client.id
 					// pour ne pas avoir plusieurs fois le même member dans
 					// le cas de multiple rôle
-				$link->bindParam(':ID', $this->ID, PDO::PARAM_INT);
+				$link->bindParam(':id', $this->id, PDO::PARAM_INT);
 				$data = $link->fetchAll(true);
 				if ($data)
 				{
@@ -185,11 +234,13 @@
 					foreach ($data as &$entry)
 					{
 						$client = new User(false);
-						$client->setID($entry['clientID']);
+						$client->setId($entry['clientId']);
 						$client->setEmail(DB::fromDB($entry['clientEmail']));
 						$client->setPassword($entry['clientPassword']);
 						$client->setName(DB::fromDB($entry['clientName']));
 						$client->setRegisterDate($entry['clientRegisterDate']);
+						$client->setLanguage(DB::fromDB($data['clientLanguage']));
+						$client->setPrivate($data['clientPrivate']);
 						$clients[] = $client;
 					}
 					return $clients;
@@ -201,7 +252,7 @@
 				return false;
 		}
 
-		function changeName($newname, $db = null)
+		function changeName($newName, $db = null)
 		{
 			$link = $this->getLink($db);
 			if ($link)
@@ -209,13 +260,55 @@
 				$link->prepare('
 					UPDATE organization
 					SET organization.name = :name
-					WHERE organization.id = :ID');
-				$tmp = DB::toDB($newname);
+					WHERE organization.id = :id');
+				$tmp = DB::toDB($newName);
 				$link->bindParam(':name', $tmp, PDO::PARAM_STR);
-				$link->bindParam(':ID', $this->ID, PDO::PARAM_INT);
+				$link->bindParam(':id', $this->id, PDO::PARAM_INT);
 				if ($link->execute(true))
 				{
-					$this->name = $newname;
+					$this->name = $newName;
+					return true;
+				}
+				else
+					return false;
+			}
+			else
+				return false;
+		}
+
+		function changeLanguage($newLanguage, $db = null)
+		{
+			$link = $this->getLink($db);
+			if (!$link)
+				throw new UnknownException("Something wrong happened", Response::UNKNOWN);
+			if (is_object($newLanguage))
+				$tmpLanguage = DB::toDB($newLanguage->code);
+			else
+				$tmpLanguage = DB::toDB($newLanguage);
+			$link->prepare('
+				UPDATE organization
+				SET organization.language = :language
+				WHERE organization.id = :id');
+			$link->bindParam(':language', $tmpLanguage, PDO::PARAM_STR);
+			$link->bindParam(':id', $this->id, PDO::PARAM_INT);
+			$link->execute(true);
+			$this->setLanguage($newLanguage);
+		}
+
+		function changePrivate($newPrivate, $db = null)
+		{
+			$link = $this->getLink($db);
+			if ($link)
+			{
+				$link->prepare('
+					UPDATE organization
+					SET organization.private = :private
+					WHERE organization.id = :id');
+				$link->bindParam(':private', (int) $newPrivate, PDO::PARAM_INT);
+				$link->bindParam(':id', $this->id, PDO::PARAM_INT);
+				if ($link->execute(true))
+				{
+					$this->setPrivate($newPrivate);
 					return true;
 				}
 				else
@@ -228,34 +321,29 @@
 		function create($db = null)
 		{
 			$link = $this->getLink($db);
-			if ($link)
-			{
-				if ($this->checkForCreation($link) == ERR::OK)
-				{
-					$link->prepare('
-						INSERT INTO organization(name)
-						VALUE(:name)');
-					$tmpname = DB::toDB($this->name);
-					$link->bindParam(':name', $tmpname, PDO::PARAM_STR);
-					if ($link->execute(true))
-						return $this->getFromID($link->link->lastInsertId());
-					else
-						return false;
-				}
-				else
-					return false;
-			}
-			else
+			if (!$link)
+				throw new UnknownException("Something wrong happened", Response::UNKNOWN);
+			$this->checkForCreation($link);
+			$link->prepare('
+				INSERT INTO organization(name, language)
+				VALUE(:name, :language)');
+			$tmpName = DB::toDB($this->name);
+			$tmpLanguage = DB::toDB($this->language->code);
+			$link->bindParam(':name', $tmpName, PDO::PARAM_STR);
+			$link->bindParam(':language', $tmpLanguage, PDO::PARAM_STR);
+			if (!$link->execute(true))
 				return false;
+			$this->getFromId($link->link->lastInsertId());
+			return true;
 		}
 
 		function checkForCreation($db = null)
 		{
 			$link = $this->getLink($db);
 			if (!$link)
-				throw new UnknownException('Something wrong append', Response::UNKNOWN);
+				throw new UnknownException("Something wrong happened", Response::UNKNOWN);
 			$link->prepare('
-				SELECT organization.id AS organizationID
+				SELECT organization.id AS organizationId
 				FROM organization
 				WHERE organization.name = :name');
 			$link->bindParam(':name', $this->name, PDO::PARAM_STR);
@@ -264,7 +352,12 @@
 				throw new ParametersException("Organization name already in use", Response::ORGNAMEUSED);
 		}
 
-		function createRole($name, $desc = "", $db = null)
+		/*
+		** Functions for advanced permision system.
+		** Could be usefull for future feature
+		*/
+		
+		/*function createRole($name, $desc = "", $db = null)
 		{
 			$link = $this->getLink($db);
 			if ($link)
@@ -276,7 +369,7 @@
 				$desc = DB::toDB($desc);
 				$link->bindParam(':name', $name, PDO::PARAM_STR);
 				$link->bindParam(':desc', $desc, PDO::PARAM_STR);
-				$link->bindParam(':id', $this->ID, PDO::PARAM_INT);
+				$link->bindParam(':id', $this->id, PDO::PARAM_INT);
 				if ($link->execute(true))
 					return $link->link->lastInsertId();
 				else
@@ -286,22 +379,22 @@
 				return false;
 		}
 
-		function addPrivForRole($roleID, $privID, $db = null)
+		function addPrivForRole($roleId, $privId, $db = null)
 		{
 			$link = $this->getLink($db);
 			if ($link)
 			{
 				$link->prepare('
 					INSERT INTO or_role_privilege(id_role, id_privilege) VALUE (:role, :priv)');
-				$link->bindParam(':role', $roleID, PDO::PARAM_INT);
-				$link->bindParam(':priv', $privID, PDO::PARAM_INT);
+				$link->bindParam(':role', $roleId, PDO::PARAM_INT);
+				$link->bindParam(':priv', $privId, PDO::PARAM_INT);
 				return $link->execute(true);
 			}
 			else
 				return false;
 		}
 
-		function removePrivForRole($roleID, $privID, $db = null)
+		function removePrivForRole($roleId, $privId, $db = null)
 		{
 			$link = $this->getLink($db);
 			if ($link)
@@ -309,15 +402,15 @@
 				$link->prepare('
 					DELETE FROM or_role_privilege
 					WHERE or_role_privilege.id_role = :role AND or_role_privilege.id_privilege = :priv');
-				$link->bindParam(':role', $roleID, PDO::PARAM_INT);
-				$link->bindParam(':priv', $privID, PDO::PARAM_INT);
+				$link->bindParam(':role', $roleId, PDO::PARAM_INT);
+				$link->bindParam(':priv', $privId, PDO::PARAM_INT);
 				return $link->execute(true);
 			}
 			else
 				return false;
 		}
 
-		function togglePrivForRole($roleID, $privID, $db = null)
+		function togglePrivForRole($roleId, $privId, $db = null)
 		{
 			$link = $this->getLink($db);
 			if ($link)
@@ -326,19 +419,19 @@
 					SELECT 1
 					FROM or_role_privilege
 					WHERE or_role_privilege.id_role = :role ADN or_role_privilege.id_privilege = :priv');
-				$link->bindParam(':role', $roleID, PDO::PARAM_INT);
-				$link->bindParam(':priv', $privID, PDO::PARAM_INT);
+				$link->bindParam(':role', $roleId, PDO::PARAM_INT);
+				$link->bindParam(':priv', $privId, PDO::PARAM_INT);
 				$ret = $link->fetch(true);
 				if ($ret)
-					return addPrivForRole($roleID, $privID, $link);
+					return removePrivForRole($roleId, $privId, $link);
 				else
-					return removePrivForRole($roleID, $privID, $link);
+					return addPrivForRole($roleId, $privId, $link);
 			}
 			else
 				return false;
 		}
 
-		function addRoleToUser($roleID, $userID, $db = null)
+		function addRoleToUser($roleId, $userId, $db = null)
 		{
 			$link = $this->getLink($db);
 			if ($link)
@@ -346,30 +439,85 @@
 				$link->prepare('
 					INSERT INTO or_user_role(id_user, id_role, id_organization)
 					VALUES(:user, :role, :org)');
-				$link->bindParam(':user', $userID, PDO::PARAM_INT);
-				$link->bindParam(':role', $roleID, PDO::PARAM_INT);
-				$link->bindParam(':org', $this->ID, PDO::PARAM_INT);
+				$link->bindParam(':user', $userId, PDO::PARAM_INT);
+				$link->bindParam(':role', $roleId, PDO::PARAM_INT);
+				$link->bindParam(':org', $this->id, PDO::PARAM_INT);
 				return $link->execute(true);
 			}
 			else
 				return false;
+		}*/
+
+		function addUserRole($roleId, $userId, $db = null)
+		{
+			$link = $this->getLink($db);
+			if (!$link)
+				throw new UnknownException("Something wrong happened", Response::UNKNOWN);
+			$link->prepare('
+				INSERT INTO client_role(id_client, id_role, categorie_target, id_target)
+				VALUE(:user, :role, "Organisation", :org)');
+			$link->bindParam(':user', $userId, PDO::PARAM_INT);
+			$link->bindParam(':role', $roleId, PDO::PARAM_INT);
+			$link->bindParam(':org', $this->id, PDO::PARAM_INT);
+			return $link->execute(true);
+		}
+
+		function removeUserRole($roleId, $userId, $db = null)
+		{
+			$link = $this->getLink($db);
+			if (!$link)
+				throw new UnknownException("Something wrong happened", Response::UNKNOWN);
+			$link->prepare('
+				DELETE FROM client_role
+				WHERE client_role.categorie_target = "Organisation"
+				AND client_role.id_target = :org
+				AND client_role.id_client = :user
+				AND client_role.id_role = :role');
+			$link->bindParam(':org', $this->id, PDO::PARAM_INT);
+			$link->bindParam(':user', $userId, PDO::PARAM_INT);
+			$link->bindParam(':role', $roleId, PDO::PARAM_INT);
+			return $link->execute(true);
+		}
+
+		function toggleUserRole($roleId, $userId, $db = null)
+		{
+			$link = $this->getLink($db);
+			if (!$link)
+				throw new UnknownException("Something wrong happened", Response::UNKNOWN);
+			$link->prepare('
+				SELECT 1
+				FROM client_role
+				WHERE client_role.categorie_target = "Organisation"
+				AND client_role.id_target = :org
+				AND client_role.id_client = :user
+				AND client_role.id_role = :role');
+			$link->bindParam(':org', $this->id, PDO::PARAM_INT);
+			$link->bindParam(':user', $userId, PDO::PARAM_INT);
+			$link->bindParam(':role', $roleId, PDO::PARAM_INT);
+			$ret = $link->fetch(true);
+			if ($ret)
+				return removeUserRole($roleId, $userId, $link);
+			else
+				return addUserRole($roleId, $userId, $link);
 		}
 
 		function delete($db = null)
 		{
 			$link = $this->getLink($db);
-			if ($link)
-			{
-				$link->prepare('
-					DELETE
-					FROM organization
-					WHERE organization.id = :ID');
-				$link->bindParam(':ID', $this->ID, PDO::PARAM_INT);
-				//Ajouter la suppression des rôles pour l'organization en question
-				return $link->execute(true);
-			}
-			else
-				return false;
+			if (!$link)
+				throw new UnknownException("Something wrong happened", Response::UNKNOWN);
+			$link->prepare('
+				DELETE
+				FROM organization
+				WHERE organization.id = :id');
+			$link->bindParam(':id', $this->id, PDO::PARAM_INT);
+			$link->execute(true);
+			$link->prepare('
+				DELETE
+				FROM client_role
+				WHERE client_role.categorie_target = "Organisation" AND client_role.id_target = :id');
+			$link->bindParam(':id', $this->id, PDO::PARAM_INT);
+			$link->execute(true);
 		}
 	}
 ?>
