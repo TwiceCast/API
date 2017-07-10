a88b32ef5795bb91c6a2228cc3917ffb5fb08c4d
Elfennacht <christian.boisson@epitech.eu>
2017-07-10 11:24:22 +0200

    GET /organizations & GET /organizations/:id
    Add:
    - Adding authentication support
    - Limit and Start parameters

--- query/GET/organizations/organization.php
+++ query/GET/organizations/organization.php
@@ -2,24 +2,48 @@
 	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Response.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Exception.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Organization.php');
+	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Authentication.php');
 
 	$response = new Response(Response::OK);
 	try {
-		$out = null;
 		$organization = new Organization();
+		$authentication = null;
 
 		if (isset($_GET['accept']))
 			$response->setContentType($_GET['accept']);
+		$authentication = new Authentication();
+		$authentication->verify(false);
+		
 		if (isset($_GET['id']))
 		{
-			if (!$organization->getFromID($_GET['id']))
+			if (!$organization->getFromId($_GET['id']))
 				throw new ParametersException("This organization does not exist", Response::DOESNOTEXIST);
 			$response->setMessage($organization);
 		}
 		else
 		{
-			$organizations = $organization->getAllOrganizations();
-			$response->setMessage(["organizations" => $organizations]);
+			if (isset($_GET['limit']))
+			{
+				if (isset($_GET['start']))
+					$organizations = $organization->getAllOrganizations($_GET['limit'], $_GET['start']);
+				else
+					$organizations = $organization->getAllOrganizations($_GET['limit']);
+			}
+			else
+				$organizations = $organization->getAllOrganizations();
+
+			$rep = new stdClass();
+			if ($organizations === false)
+			{
+				$rep->organization_list = null;
+				$rep->organization_total = 0;
+			}
+			else
+			{
+				$rep->organization_list = $organizations;
+				$rep->organization_total = count($organizations);
+			}
+			$response->setMessage($rep);
 		}
 	} catch (CustomException $e) {
 		$response->setError($e);
