1c8e8a1c87d5a157acb80ec7507b767fd31173ad
Elfennacht <christian.boisson@epitech.eu>
2017-07-10 15:03:16 +0200

    PUT /organizations/:id - Implemented

--- query/PUT/organizations/organization.php
+++ query/PUT/organizations/organization.php
@@ -1,12 +1,48 @@
 <?php
+	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Authentication.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Response.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Exception.php');
-
+	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Organization.php');
+	require_once($_SERVER['DOCUMENT_ROOT'].'/class/functions.php');
+	
 	$response = new Response(Response::OK);
 	try {
+		$postdata = getPostData();
 		if (isset($_GET['accept']))
 			$response->setContentType($_GET['accept']);
-		throw new NotImplementedException("This feature is not implemented yet", 501);
+		
+		$authentication = new Authentication();
+		$authentication->verify();
+		$user = $authentication->getUserFromToken();
+		
+		if (!$user)
+			throw new UnknownException("Something wrong happened", Response::UNKNOWN);
+		
+		if (!isset($_GET['id']))
+			throw new ParametersException("Missing parameters to proceed", Response::MISSPARAM);
+		
+		$organization = new Organization();
+		$organization->getFromId($_GET['id']);
+		
+		if (!$organization)
+			throw NotFoundException("This organization does not exist", Response::NOTFOUND);
+		
+		// role 4 == Organization Founder
+		if ($authentication->userHasRights(4, $organization->id) === false)
+			throw new ParametersException("You cannot modify someone else's organization", Response::NORIGHT);
+		
+		if (!isset($postdata['name']) or !isset($postdata['language']))
+			throw new ParametersException("Missing parameters to proceed", Response::MISSPARAM);
+
+		$organization->setName($postdata['name']);
+		$organization->setLanguage($postdata['language']);
+		if (!isset($postdata['private']))
+			$postdata['private'] = false;
+		$organization->setPrivate($postdata['private']);
+		
+		if (!$user->update())
+			throw new UnknownException("Something wrong happened", Response::UNKNOWN);
+		$response->setMessage($organization);
 	} catch (CustomException $e) {
 		$response->setError($e);
 	} finally {
