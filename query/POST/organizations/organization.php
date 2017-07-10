492f092bbe3dbdb6ea57a4f20b454f8ada603a9d
Elfennacht <christian.boisson@epitech.eu>
2017-07-10 13:46:23 +0200

    POST /organization - Implemented

--- query/POST/organizations/organization.php
+++ query/POST/organizations/organization.php
@@ -1,17 +1,40 @@
 <?php
+	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Authentication.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Response.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Exception.php');
+	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Organization.php');
+	require_once($_SERVER['DOCUMENT_ROOT'].'/class/functions.php');
 
 	$response = new Response(Response::OK);
 	try {
+		$postdata = getPostData();
 		if (isset($_GET['accept']))
 			$response->setContentType($_GET['accept']);
-		throw new NotImplementedException("This feature is not implemented yet", 501);
+
+		$authentication = new Authentication();
+		$authentication->verify();
+		$user = $authentication->getUserFromToken();
+		
+		if (!$user)
+			throw new UnkownException("Something wrong happened");
+		
+		if (!isset($postdata['name']) or !isset($postdata['language']))
+			throw new ParametersException("Missing parameters", Response::MISSPARAM);
+		
+		$organization = new Organization();
+		$organization->setName($postdata['name']);
+		$organization->setLanguage($postdata['language']['code']);
+		if (!isset($postdata['private']))
+			$postdata['private'] = false;
+		$organization->setPrivate($postdata['private']);
+		
+		if (!$organization->create($user->id))
+			throw new UnkownException("Something wrong happened", Response::UNKNOWN);
+		
+		$response->setMessage($organization);
 	} catch (CustomException $e) {
 		$response->setError($e);
 	} finally {
 		$response->send();
 	}
-	// echo "POSTORGANIZATION.PHP";
-	// var_dump($_GET);
 ?>
