71066f60b74602083d5a2a0fe35ac578537fe741
Elfennacht <christian.boisson@epitech.eu>
2017-07-10 16:00:21 +0200

    PATCH /organizations/:id - Implemented

--- query/PATCH/organizations/organization.php
+++ query/PATCH/organizations/organization.php
@@ -1,17 +1,46 @@
 <?php
+	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Authentication.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Response.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Exception.php');
+	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Organization.php');
+	require_once($_SERVER['DOCUMENT_ROOT'].'/class/functions.php');
 
 	$response = new Response(Response::OK);
 	try {
+		$postdata = getPostData();
 		if (isset($_GET['accept']))
 			$response->setContentType($_GET['accept']);
-		throw new NotImplementedException("This feature is not implemented yet", 501);
+		
+		$authentication = new Authentication();
+		$authentication->verify();
+		$user = $authentication->getUserFromToken();
+		
+		if (!$user)
+			throw new UnknownException("Something wrong happened", Response::UNKNOWN);
+		
+		if (!isset($_GET['id']))
+			throw new ParametersException("Missing parameters to proceed", Response::MISSPARAM);
+		
+		$organization = new Organization;
+		$organization->getFromId($_GET['id']);
+		
+		if (!$organization)
+			throw NotFoundException("This organization does not exist", Response::NOTFOUND);
+		
+		// role 4 == Organization Founder
+		if ($authentication->userHasRights(4, $organization->id) === false)
+			throw new RightsException("You cannot modify someone else's organization", Response::NORIGHT);
+		if (isset($postdata['name']))
+			$organization->changeName($postdata['name']);
+		if (isset($postdata['language']))
+			$organization->changeLanguage($postdata['language']);
+		if (isset($postdata['private']))
+			$organization->changePrivate($postdata['private']);
+
+		$response->setMessage($organization);
 	} catch (CustomException $e) {
 		$response->setError($e);
 	} finally {
 		$response->send();
 	}
-	// echo "PATCHORGANIZATION.PHP";
-	// var_dump($_GET);
 ?>
