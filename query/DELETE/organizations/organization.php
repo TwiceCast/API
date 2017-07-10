3d65ffa338fe8f40f45908dafba441b7d7869466
Elfennacht <christian.boisson@epitech.eu>
2017-07-10 17:40:29 +0200

    DELETE /organization/:id - Fully recast

--- query/DELETE/organizations/organization.php
+++ query/DELETE/organizations/organization.php
@@ -1,27 +1,34 @@
 <?php
+	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Authentication.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Response.php');
-	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Exception.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Exception.php');
 	require_once($_SERVER['DOCUMENT_ROOT'].'/class/Organization.php');
 
 	$response = new Response();
 	try {
-		$out = null;
-		$organization = new Organization();
-
 		if (isset($_GET['accept']))
 			$response->setContentType($_GET['accept']);
+		$authentication  = new Authentication();
+		$authentication->verify();
+		
 		if (!isset($_GET['id']))
 			throw new ParametersException("Missing parameters to proceed", Response::MISSPARAM);
-		if ($organization->getFromID($_GET['id']))
-		{
-			if ($organization->delete())
-				$response->setMessage(["message" => "Organization deleted successfully"], Response::SUCCESS);
-			else
-				throw new UnknownException("Something wrong happened", Response::UNKNOWN);
-		}
-		else
-			throw new ParametersException("This organization does not exist", Response::DOESNOTEXIST);
+
+		$user = $authentication->getUserFromToken();
+		if (!$user)
+			throw new UnknownException("Something wrong happened", Response::UNKNOWN);
+		
+		$organization = new Organization();
+		if ($organization->getFromId($_GET['id']) === false)
+			throw new NotFoundException("This organization does not exist", Response::NOTFOUND);
+		
+		// role 4 == Organization Founder
+		if ($authentication->userHasRights(4, $organization->id) === false)
+			throw new RightsException("You cannot modify someone else's organization", Response::NORIGHT);
+		
+		if (!$organization->delete())
+			throw new UnknownException("Something wrong happened", Response::UNKNOWN);
+		$response->setMessage("The organization has been removed successfully", Response::SUCCESS);
 	} catch (CustomException $e) {
 		$response->setError($e);
 	} finally {
