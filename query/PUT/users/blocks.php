0928424894df335ea9f684df4180012e5564af51
Elfennacht <christian.boisson@epitech.eu>
2018-01-18 17:47:00 +0100

    PUT /users/:id/blocks/:block/ - Mark an User as Block
    PUT /users/:id/blocks/:block/
    Mark an User as Block for a specific User

---
+++ query/PUT/users/blocks.php
@@ -0,0 +1,36 @@
+<?php
+    require_once($_SERVER['DOCUMENT_ROOT'].'/class/Block.php');
+    require_once($_SERVER['DOCUMENT_ROOT'].'/class/User.php');
+    require_once($_SERVER['DOCUMENT_ROOT'].'/class/Authentication.php');
+    require_once($_SERVER['DOCUMENT_ROOT'].'/class/Response.php');
+    require_once($_SERVER['DOCUMENT_ROOT'].'/class/Exception.php');
+
+    $response = new Response(Response::OK);
+    try {
+        if (isset($_GET['accept']))
+            $response->setContentType($_GET['accept']);
+        $authentication = new Authentication();
+        $authentication->verify();
+        if (!isset($_GET['id']) || !isset($_GET['block']))
+            throw new ParametersException("Missing parameters to proceed", Response::MISSPARAM);
+        $user = $authentication->getUserFromToken();
+
+        if (!$user)
+            throw new UnknownException("Something wrong happened", Response::UNKNOWN);
+        if ($user->id != $_GET['id'])
+            throw new RightsException("You cannot modify someone else's blocks", Response::NORIGHT);
+        $block = new User();
+        if (!$block->getFromId($_GET['block']))
+            throw new NotFoundException("The 'block' user doesn't exist", Response::NOTFOUND);
+
+        $b = new Block();
+        if (!$b->addBlockById($user->id, $block->id))
+            throw new UnknownException("Something wrong happened", Response::UNKNOWN);
+        
+        $response->setMessage($user, Response::CREATED);
+    } catch (CustomException $e) {
+        $response->setError($e);
+    } finally {
+        $response->send();
+    }
+?>
