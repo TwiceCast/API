f5a367e591725554cb8b5e6b0f9af8ee97b9dbcb
Elfennacht <christian.boisson@epitech.eu>
2018-01-18 16:54:30 +0100

    GET /users/:id/friends/ - Get a list of user marked as friend
    GET /users/:id/friends/
    Retrieve the list of Users marked as friend for a specific User

--- query/GET/users/friends.php
+++ query/GET/users/friends.php
@@ -1,1 +1,41 @@
-<?php echo "GET" ?>
+<?php
+    require_once($_SERVER['DOCUMENT_ROOT'].'/class/Friend.php');
+    require_once($_SERVER['DOCUMENT_ROOT'].'/class/Authentication.php');
+    require_once($_SERVER['DOCUMENT_ROOT'].'/class/Response.php');
+    require_once($_SERVER['DOCUMENT_ROOT'].'/class/Exception.php');
+
+    $response = new Response(Response::OK);
+    try {
+        if (isset($_GET['accept']))
+            $response->setContentType($_GET['accept']);
+        $authentication = new Authentication();
+        $authentication->verify();
+        if (!isset($_GET['id']))
+            throw new ParametersException("Missing parameters to proceed", Response::MISSPARAM);
+        $user = $authentication->getUserFromToken();
+
+        if (!$user)
+            throw new UnknownException("Something wrong happened", Response::UNKNOWN);
+        if ($user->id != $_GET['id'])
+            throw new RightsException("You cannot access someone else's friends", Response::NORIGHT);
+        $f = new Friend();
+        $friends = $f->getFromId($user->id);
+
+        $rep = new stdClass();
+        if ($friends === false)
+        {
+            $rep->user_list = null;
+            $rep->user_total = 0;
+        }
+        else
+        {
+            $rep->user_list = $friends;
+            $rep->user_total = count($friends);
+        }
+        $response->setMessage($rep);
+    } catch (CustomException $e) {
+        $response->setError($e);
+    } finally {
+        $response->send();
+    }
+?>
