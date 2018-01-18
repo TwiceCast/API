6310a6987f40189b38bd0d68809cfb1be5d9d2a5
Elfennacht <christian.boisson@epitech.eu>
2018-01-18 16:56:47 +0100

    DELETE /users/:id/friends/:friend/ - Unmark an User as Friend
    DELETE /users/:id/friends/:friend/
    Unmark an User as Friend for a specific User

--- query/DELETE/users/friends.php
+++ query/DELETE/users/friends.php
@@ -1,1 +1,37 @@
-<?php echo "DELETE" ?>
+<?php
+    require_once($_SERVER['DOCUMENT_ROOT'].'/class/Friend.php');
+    require_once($_SERVER['DOCUMENT_ROOT'].'/class/User.php');
+    require_once($_SERVER['DOCUMENT_ROOT'].'/class/Authentication.php');
+    require_once($_SERVER['DOCUMENT_ROOT'].'/class/Response.php');
+    require_once($_SERVER['DOCUMENT_ROOT'].'/class/Exception.php');
+
+    $response = new Response(Response::OK);
+    try {
+        if (isset($_GET['accept']))
+            $response->setContentType($_GET['accept']);
+        $authentication = new Authentication();
+        $authentication->verify();
+        if (!isset($_GET['id']) || !isset($_GET['friend']))
+            throw new ParametersException("Missing parameters to proceed", Response::MISSPARAM);
+        $user = $authentication->getUserFromToken();
+        
+        if (!$user)
+            throw new UnknownException("Something wrong happened", Response::UNKNOWN);
+        if ($user->id != $_GET['id'])
+            throw new RightsException("You cannot modify someone else's friends", Response::NORIGHT);
+        $friend = new User();
+        if (!$friend->getFromId($_GET['friend']))
+            throw new NotFoundException("the 'friend' user doesn't exist", Response::NOTFOUND);
+
+        $f = new Friend();
+        if (!$f->removeFriendById($user->id, $friend->id))
+            throw new UnknownException("Something wrong happened", Response::UNKNOWN);
+
+        //$response->setMessage(null, Response::NOCONTENT);
+        $response->setResponseCode(Response::NOCONTENT);
+    } catch (CustomException $e) {
+        $response->setError($e);
+    } finally {
+        $response->send();
+    }
+?>
